b199238671925f1da02a882970ad50d34da21ea1
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:52:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,5 +34,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.names, COUNT(b"
 end
