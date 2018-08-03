52f9eea304da304dcbd89f4f588d9827e62caf19
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:46:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors INNER JOIN series on author.id = series.author_id
-  INNER JOIN subgenres ON "
+  INNER JOIN subgenres ON series."
 end
 
 def select_series_title_with_most_human_characters
