26215c1ecc4d50487e2e433661efa94383fa9c36
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:11:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year"
+  "SELECT books.title, books.year
+  FROM books 
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
