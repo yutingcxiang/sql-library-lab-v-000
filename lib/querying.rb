2607ee44134b9d6abf5a8d85f16020e1777d96a4
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:50:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
-  FROM books INNER JOIN series on books.series_id = series.id
+  FROM books INNER JOIN series ON books.series_id = series.id
   WHERE books.series_id = 1
   ORDER BY books.year"
 end
@@ -21,12 +21,16 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors INNER JOIN series on authors.id = series.author_id
+  FROM authors INNER JOIN series ON authors.id = series.author_id
   INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title"
+  "SELECT series.title
+  FROM series INNER JOIN characters ON series.id = characters.series_id
+  GROUP BY characters.species
+  HAVING characters.species = 'human'
+  SORT BY COUNT(species) LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
