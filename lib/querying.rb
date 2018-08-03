4fc7b3fdd3c57824288e62cedd421d28eea03193
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:19:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
-  FROM books INNER JOIN series on books.id = series.book_id
+  FROM books INNER JOIN series on books.series_id = series.id
   WHERE books.series_id = 1
   ORDER BY books.year"
 end
