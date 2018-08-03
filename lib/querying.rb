645b5d8dca1105ea68b812cfaa6aa9eab589d8e5
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:27:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT "
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
