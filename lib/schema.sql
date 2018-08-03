b0649ce0a2238edeeb0c9dd47ab0e25ffbdca369
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 05:59:26 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -30,3 +30,10 @@ CREATE TABLE characters (
   series_id INTEGER,
   author_id INTEGER,
 );
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER,
+);
+
