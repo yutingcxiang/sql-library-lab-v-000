d572142e881998b5ff8166be9fdcf7b6629e31e3
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:58:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,6 +29,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series INNER JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
+  GROUP BY characters.species
   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
