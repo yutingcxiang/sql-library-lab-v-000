8d597d41fe7587c9e70f252b9a81a4998c7e07a9
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 06:22:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  ""
+  "SELECT name, motto
+  FROM CHARACTERS
+  HAVING MAX("
 end
 
 
