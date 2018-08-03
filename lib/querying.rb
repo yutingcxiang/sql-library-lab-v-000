ce5cf0ca92a46077275db235d8489b3f59e32d6e
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 07:12:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
   FROM series INNER JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(species) DESC LIMIT 1"
+  ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
