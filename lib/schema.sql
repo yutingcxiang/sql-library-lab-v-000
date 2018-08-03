cde5fa38db0cc2ca1037ec8ccb6ace573c62c86d
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 05:57:00 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,11 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  author_id TEXT,
-  subgenre_id TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER,
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
 );
